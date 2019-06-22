ed367b9e5a41cd820f18d642ba35b5309307eb99
Murali Krishnan <murali.govindarajulu@imaginea.com>
2019-06-22 16:23:12 +0530

    Initial Commit

---
+++ iOSExamples/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSExamples
+//
+//  Created by Murali Krishnan on 22/06/19.
+//  Copyright © 2019 Murali Krishnan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
